964f28d9faec1a3d7ea248012ff58341a11d7854
txya900619 <wayne900619@gmail.com>
2020-02-28 13:17:55 +0800

    add 9.v-model

---
+++ 9.v-model/index.js
@@ -0,0 +1,12 @@
+app = new Vue({
+  el: "#app",
+  data: {
+    picked: "One",
+    toggle1: true,
+    toggle2: "noooooo",
+    pick1: "a",
+    a: { text: "olaaaaaaaa", num: 1 },
+    pick2: this.a,
+    msg: ""
+  }
+});
